97054e48c847c23a618a79681fe7fe3385d42d60
ppandey <ppandey@umcom.org>
2026-02-08 09:09:12 -0600

    asset updatet

--- Plugins/AegisMotion/Source/AegisMotionEditor/Private/AegisMotionEditorModule.cpp
+++ Plugins/AegisMotion/Source/AegisMotionEditor/Private/AegisMotionEditorModule.cpp
@@ -1,8 +1,1 @@
 #include "Modules/ModuleManager.h"
-
-class FAegisMotionEditorModule final : public IModuleInterface
-{
-public:
-    virtual void StartupModule() override {}
-    virtual void ShutdownModule() override {}
-};
